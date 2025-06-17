b43559b9b4c1b2ea7b39131ec1ffcfbab811c988
Abduvali Toshkentov <toshkentovabduvali@gmail.com>
2025-06-17 19:25:14 +0500

    проэкт создаль

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	users := map[string]map[string]int{
+		"user1": {
+			"ZyWoo1":  1,
+			"m0NESY2": 2,
+			"Donk3":   3,
+			"Simleo4": 4,
+			"nIKo5":   5,
+		},
+		"user2": {
+			"ZyWoo1":  1,
+			"m0NESY2": 2,
+			"Donk3":   3,
+			"Simleo4": 4,
+			"nIKo5":   5,
+		},
+	}
+	for k, v := range users["user1"] {
+		fmt.Printf("key: %s, value: %d\n", k, v)
+	}
+	for k, v := range users["user2"] {
+		fmt.Printf("key: %s, value: %d\n", k, v)
+	}
+}
