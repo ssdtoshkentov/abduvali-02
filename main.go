11918cc7d7aa9b37ef935848da3bdbf9acf73613
Abduvali Toshkentov <toshkentovabduvali@gmail.com>
2025-06-18 16:24:26 +0500

    Simpleo5 Abduvali's n1 cs2 player in 13 years old

--- main.go
+++ main.go
@@ -11,11 +11,11 @@ func main() {
 		"batyor5":  5,
 	}
 	user2 := map[string]int{
-		"ZyWoo1":   1,
-		"m0NESY2":  2,
-		"Donk3":    3,
-		"Simpleo5": 5,
-		"baxtyor5": 5,
+		"ZyWoo1":                    1,
+		"m0NESY2":                   2,
+		"Donk3":                     3,
+		"Abduvali's n1 cs2 player5": 5,
+		"baxtyor5":                  5,
 	}
 	for k, v := range user1 {
 		fmt.Printf("user1 key: %s, value: %d\n", k, v)
