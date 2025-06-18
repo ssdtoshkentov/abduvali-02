b667a521973f47217ba5e8ef288df33c26cfddf6
Abduvali Toshkentov <toshkentovabduvali@gmail.com>
2025-06-18 16:19:44 +0500

    Simpleo4 Simpleo5

--- main.go
+++ main.go
@@ -3,25 +3,24 @@ package main
 import "fmt"
 
 func main() {
-
-	"user1" := map[string]int{
+	user1 := map[string]int{
 		"ZyWoo1":  1,
 		"m0NESY2": 2,
 		"Donk3":   3,
 		"Simleo4": 4,
 		"batyor5": 5,
 	}
-	"user2" := map[string]int{
+	user2 := map[string]int{
 		"ZyWoo1":   1,
 		"m0NESY2":  2,
 		"Donk3":    3,
 		"Simleo4":  4,
 		"baxtyor5": 5,
 	}
-	for k, v := range "user1" {
+	for k, v := range user1 {
 		fmt.Printf("user1 key: %s, value: %d\n", k, v)
 	}
-	for k, v := range "user2" {
+	for k, v := range user2 {
 		fmt.Printf("user2 key: %s, value: %d\n", k, v)
 	}
 }
