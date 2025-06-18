aae0f724d79cc97adafbe40aee224eabf80f79b4
Abduvali Toshkentov <toshkentovabduvali@gmail.com>
2025-06-18 23:00:00 +0500

    baxtyor5 nIKo5

--- main.go
+++ main.go
@@ -8,14 +8,14 @@ func main() {
 		"m0NESY2":  2,
 		"Donk3":    3,
 		"Simpleo4": 4,
-		"baxtyor5": 5,
+		"nIKo5":    5,
 	}
 	user2 := map[string]int{
 		"ZyWoo1":   1,
 		"m0NESY2":  2,
 		"Donk3":    3,
 		"Simpleo4": 4,
-		"batyor5":  5,
+		"nIKo5":    5,
 	}
 	for k, v := range user1 {
 		fmt.Printf("user1 key: %s, value: %d\n", k, v)
