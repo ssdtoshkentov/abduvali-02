74afae521280fecb0dbf3c2e4460c17b6d776839
Abduvali Toshkentov <toshkentovabduvali@gmail.com>
2025-06-18 16:14:11 +0500

    nIKo5 baxtyor5

--- main.go
+++ main.go
@@ -4,26 +4,24 @@ import "fmt"
 
 func main() {
 
-	users := map[string]map[string]int{
-		"user1": {
-			"ZyWoo1":  1,
-			"m0NESY2": 2,
-			"Donk3":   3,
-			"Simleo4": 4,
-			"nIKo5":   5,
-		},
-		"user2": {
-			"ZyWoo1":  1,
-			"m0NESY2": 2,
-			"Donk3":   3,
-			"Simleo4": 4,
-			"nIKo5":   5,
-		},
+	"user1" := map[string]int{
+		"ZyWoo1":  1,
+		"m0NESY2": 2,
+		"Donk3":   3,
+		"Simleo4": 4,
+		"batyor5": 5,
 	}
-	for k, v := range users["user1"] {
-		fmt.Printf("key: %s, value: %d\n", k, v)
+	"user2" := map[string]int{
+		"ZyWoo1":   1,
+		"m0NESY2":  2,
+		"Donk3":    3,
+		"Simleo4":  4,
+		"baxtyor5": 5,
 	}
-	for k, v := range users["user2"] {
-		fmt.Printf("key: %s, value: %d\n", k, v)
+	for k, v := range "user1" {
+		fmt.Printf("user1 key: %s, value: %d\n", k, v)
+	}
+	for k, v := range "user2" {
+		fmt.Printf("user2 key: %s, value: %d\n", k, v)
 	}
 }
