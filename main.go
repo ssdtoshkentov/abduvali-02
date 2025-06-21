4883c4e78a6651ad784b0d3061d771caec7e3f46
Abduvali Toshkentov <toshkentovabduvali@gmail.com>
2025-06-21 15:20:46 +0500

    nIKo5 Mardon nima gap

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ func main() {
 		"m0NESY is the best player in 2025 year": 2,
 		"Donk is the best player in 2025 year":   3,
 		"Simple is the best player in 2025 year": 4,
-		"nIKo5":                                  5,
+		"Mardon nima gap":                        5,
 	}
 	user2 := map[string]int{
 		"ZyWoo is the best player in austin major 2025": 1,
