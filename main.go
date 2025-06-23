2ca5c139325fb2611d7954e5c1d43a50a039fa92
Abduvali Toshkentov <toshkentovabduvali@gmail.com>
2025-06-23 16:02:08 +0500

    is the best AWP player in 2025

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 	}
 	user2 := map[string]int{
 		"ZyWoo is the best player in austin major 2025": 1,
-		"m0NESY is th best AWP player in 2025":          2,
+		"m0NESY4": 2,
 	}
 	for k, v := range user1 {
 		fmt.Printf("user1 key: %s, value: %d\n", k, v)
