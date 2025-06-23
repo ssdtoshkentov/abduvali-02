1514413159416e65d0ca03ed090c6fecbc59cf9d
Abduvali Toshkentov <toshkentovabduvali@gmail.com>
2025-06-23 16:09:15 +0500

    is the best player in 2025 year

--- main.go
+++ main.go
@@ -7,8 +7,8 @@ import (
 func main() {
 
 	user1 := map[string]int{
-		"ZyWoo is the best player in 2025 year":  1,
-		"m0NESY is the best player in 2025 year": 2,
+		"ZyWoo2":  1,
+		"m0NESY2": 2,
 	}
 	user2 := map[string]int{
 		"ZyWoo1":  1,
